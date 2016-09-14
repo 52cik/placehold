49dd0c6d5d86a61ad3bd2de49d6bb64540962334
楼教主 <cik520@qq.com>
2016-09-14 23:21:15 +0800

    support all versions of node.

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 'use strict';
 
-const RE_FORMAT = /^\/(\d+)(?:x(\d+))?(?:\/([\da-f]{6}|[\da-f]{3}))?(?:\/([\da-f]{6}|[\da-f]{3}))?(?:\??text=(.+))?$/i;
+var RE_FORMAT = /^\/(\d+)(?:x(\d+))?(?:\/([\da-f]{6}|[\da-f]{3}))?(?:\/([\da-f]{6}|[\da-f]{3}))?\/?(?:\??text=(.+))?$/i;
 
 module.exports = function (req, res) {
-  let m = req.url.match(RE_FORMAT) || {};
+  var m = req.url.match(RE_FORMAT) || {};
 
-  let width = m[1] || 100;
-  let height = m[2] || width;
-  let color = m[3] || 'aaa';
-  let color_bg = m[4] || 'eee';
-  let text = m[5] ? decodeURIComponent(m[5]) : `${width}x${height}`;
+  var width = m[1] || 100;
+  var height = m[2] || width;
+  var color_bg = m[3] || 'eee';
+  var color = m[4] || 'aaa';
+  var text = m[5] ? decodeURIComponent(m[5]) : (width + 'x' + height);
 
-  let svg = `<svg width="${width}" height="${height}" xmlns="http://www.w3.org/2000/svg">
-<rect width="${width}" height="${height}" fill="#${color_bg}"/>
-<text x="${width / 2}px" y="${height / 2}" dy=".3em" fill="#${color}" style="text-anchor:middle;font:bold 10pt arial,sans-serif">${text}</text>
-</svg>`;
+  var svg = '<svg width="' + width + '" height="' + height + '" xmlns="http://www.w3.org/2000/svg">\
+<rect width="' + width + '" height="' + height + '" fill="#' + color_bg + '"/>\
+<text x="' + (width / 2) + '" y="' + (height / 2) + '" dy=".3em" fill="#' + color + '" style="text-anchor:middle;font:bold 10pt arial,sans-serif">' + text + '</text>\
+</svg>';
 
   res.setHeader('Content-Type', 'image/svg+xml');
   res.end(svg);
