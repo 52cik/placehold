3973255507f96aa46665391dac0dde2a472e2ea2
楼教主 <cik520@qq.com>
2016-10-11 22:24:11 +0800

    fix bug

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 'use strict';
 
+var fs = require('fs');
+
 var RE_FORMAT = /^\/(\d+)(?:x(\d+))?(?:\/([\da-f]{6}|[\da-f]{3}))?(?:\/([\da-f]{6}|[\da-f]{3}))?\/?(?:\??text=(.+))?$/i;
 
 module.exports = function (req, res) {
+  if (req.url === '/') {
+    res.setHeader('Content-Type', 'text/html');
+    res.writeHead(200, 'OK');
+    fs.createReadStream('index.html').pipe(res);
+    return;
+  }
+
   var m = req.url.match(RE_FORMAT) || {};
 
   var width = m[1] || 100;
@@ -11,9 +20,8 @@ module.exports = function (req, res) {
   var color = m[4] || 'aaa';
   var text = m[5] ? decodeURIComponent(m[5]) : (width + 'x' + height);
 
-  var svg = '<svg width="' + width + '" height="' + height + '" xmlns="http://www.w3.org/2000/svg">\
-<rect width="' + width + '" height="' + height + '" fill="#' + color_bg + '"/>\
-<text x="' + (width / 2) + '" y="' + (height / 2) + '" dy=".3em" fill="#' + color + '" style="text-anchor:middle;font:bold 10pt arial,sans-serif">' + text + '</text>\
+  var svg = '<svg width="' + width + '" height="' + height + '" xmlns="http://www.w3.org/2000/svg" style="background-color:#' + color_bg + ';">\
+<text x="50%" y="50%" dy=".3em" fill="#' + color + '" style="text-anchor:middle;font:bold 15pt arial,sans-serif">' + text + '</text>\
 </svg>';
 
   res.setHeader('Content-Type', 'image/svg+xml');
