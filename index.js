d41fb599e1959429bc976db41503ed90f72e1265
楼教主 <cik520@qq.com>
2016-10-14 22:54:36 +0800

    fix bug

--- index.js
+++ index.js
@@ -6,13 +6,19 @@ var RE_FORMAT = /^\/(\d+)(?:x(\d+))?(?:\/([\da-f]{6}|[\da-f]{3}))?(?:\/([\da-f]{
 
 module.exports = function (req, res) {
   if (req.url === '/') {
-    res.setHeader('Content-Type', 'text/html');
-    res.writeHead(200, 'OK');
-    fs.createReadStream('index.html').pipe(res);
+    fs.readFile('index.html', function(err, data) {
+      if (err) throw err;
+
+      var html = data.toString().replace(/localhost:9191/g, req.headers.host);
+
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end(html);
+    });
+
     return;
   }
 
-  var m = req.url.match(RE_FORMAT) || {};
+  var m = req.url.match(RE_FORMAT) || [];
 
   var width = m[1] || 100;
   var height = m[2] || width;
@@ -20,10 +26,10 @@ module.exports = function (req, res) {
   var color = m[4] || 'aaa';
   var text = m[5] ? decodeURIComponent(m[5]) : (width + 'x' + height);
 
-  var svg = '<svg width="' + width + '" height="' + height + '" xmlns="http://www.w3.org/2000/svg" style="background-color:#' + color_bg + ';">\
+  var svg = '<svg xmlns="http://www.w3.org/2000/svg" width="' + width + '" height="' + height + '" style="width: 100%;height: 100%;margin:auto;background-color:#' + color_bg + ';" viewBox="0 0 ' + width + ' ' + height + '" preserveAspectRatio="none">\
 <text x="50%" y="50%" dy=".3em" fill="#' + color + '" style="text-anchor:middle;font:bold 15pt arial,sans-serif">' + text + '</text>\
 </svg>';
 
-  res.setHeader('Content-Type', 'image/svg+xml');
+  res.writeHead(200, {'Content-Type': 'image/svg+xml'});
   res.end(svg);
 };
